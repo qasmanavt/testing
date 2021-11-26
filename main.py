c05ffd2d0df589a19681d0563a953f0eafe55b0a
qasmanavt <qlndrvxjbk@gmail.com>
2021-11-26 21:30:21 +0500

    main

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-from contextvars import copy_context
-from functools import update_wrapper
-import json
-from logging import Logger
-from os import name,stat
-from typing import Text
-from aiogram import types
-from aiogram.types import message, message_id, user
+import threading
+import telegram
 from config import TOKEN
 from telegram import *
 from telegram.ext import *
 from requests import *
 from pictures import *
-from datetime import datetime
-from json import JSONEncoder
+from time import *
+import time
 import pyodbc
-import telebot
-
-phone_number=""
+ 
+phone_number=[]
+list=[]
+a=0
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
-bot = telebot.TeleBot(TOKEN)
-user = "register"
-customer = "Chef"
-order_status = "waiting"
-first_food = 0
-second_food = 0
-third_food = 0
+bot = telegram.Bot(TOKEN)
+password = "qwerty"
 connection = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
     "Server=LAPTOP-DFK8LR20;"
@@ -34,150 +24,101 @@ connection = pyodbc.connect(
 
 )
 def startCommand(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton(user)]]
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=" welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons))
+    name = update.effective_chat.full_name
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=" welcome to my bot! " + name)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="enter your password")
 
 
 def messageHandler(update: Update, context: CallbackContext):
-    if "register" in update.message.text:
-        context.bot.send_message(chat_id=update.effective_chat.id,text="please write your phone number")
-        print(update.message.text)
-    if "9989" in update.message.text:
-        global phone_number,first_food,second_food,third_food
-        phone_number=str(update.message.text)
-        
-        
-        button = [[KeyboardButton("buskets")], [KeyboardButton("menu")], [
-            KeyboardButton("order status")]]
+ 
 
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                text="hello my dear , thanks for using this bot", reply_markup=ReplyKeyboardMarkup(button))
+    if password == update.message.text:
+        buttons = [[KeyboardButton("reservations")], [KeyboardButton("menu")]]
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hello ,here you can see user's order!",
+                                 reply_markup=ReplyKeyboardMarkup(buttons))
 
-        image = get(url).content
-        context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[
-                                InputMediaPhoto(image, caption="")])
-
-        buttons = [[InlineKeyboardButton("1", callback_data="1")],
-                [InlineKeyboardButton("2", callback_data="2")],
-                [InlineKeyboardButton("3", callback_data="3")]]
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                reply_markup=InlineKeyboardMarkup(buttons), text="choose what food you want😊😊😊")
-
-    if "buskets" in update.message.text:
-        text = ("first food count "+str(first_food), "second food count "+str(second_food), "third food count "+str(third_food),
-                "username "+str(update.effective_chat.full_name))
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-        first_food=0
-        second_food=0
-        third_food=0
-
-    if "menu" in update.message.text:
-
-        image = get(url).content
+        image = get(url_for_chef).content
         context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[
                                    InputMediaPhoto(image, caption="")])
 
-        buttons = [[InlineKeyboardButton("1", callback_data="1st")],
-                   [InlineKeyboardButton("2", callback_data="2nd")],
-                   [InlineKeyboardButton("3", callback_data="3rd")]]
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 reply_markup=InlineKeyboardMarkup(buttons), text="choose what food you want😊😊😊")
-        cursor = connection.cursor()
+        
+    elif "reservation" in  update.message.text:
+        global phone_number,list
+        def printit():
+            threading.Timer(10.0, printit).start()
+            time.sleep(5)
+            status_text="waiting"
+            cursor = connection.cursor()
+            b2=cursor.execute(f'select status  from bot2 where status=? ;',status_text).fetchall()
+          
+            b=[]
+            for b in b2:
+                print(b)
+                
+        
+            
+                
+            
+            
+            if "waiting" in b:
+                status_text="waiting"
+                b=cursor.execute('select  *  from bot2 where status=? ;',status_text).fetchall()
+                
+                for a in b:
+                    text="first food is "+str(a[0])+"\nsecond food is "+str(a[1])+"\nthird food is "+str(a[2])+"\nname :"+str(a[3])+"\nphone number "+str(a[7])
+                    
+                 
+                    buttons2 = [[InlineKeyboardButton("aceepted", callback_data="1")], 
+                    [InlineKeyboardButton("decline", callback_data="2")]]
+                    context.bot.send_message(chat_id=update.effective_chat.id,
+                    reply_markup=InlineKeyboardMarkup(buttons2), text=text)     
+                print("uji")
+            elif b is None:
+                print("x")
+            else:
+                print("ey")
+            connection.commit()
+        printit()   
 
-        # id = update.effective_chat.id
-        # id2=cursor.execute(f'select distinct  id  from bot2   ;').fetchall()
-        # for number in id2:
-
-        #     if id in number:
-        #         phone=cursor.execute(f'select distinct  phone_number  from bot2 where id=?  ;',int(number)).fetchone()
-        #         phone_number=str(phone)
-    
-
-    if "order status" in update.message.text:
-        print("create")
-        cursor = connection.cursor()
-
-        id = update.effective_chat.id
-        cursor.execute(f'select top 1 status  from bot2 where phone_number=? order by time desc ;',phone_number
-                        )
-
-        context.bot.send_message(chat_id=update.effective_chat.id,text=cursor.fetchone()[0])
-        connection.commit()
+    elif "menu" in update.message.text:
+        context.bot.send_message(chat_id=update.effective_chat.id,text="sorry this page will be make sooner :) ")       
+ 
 
     def queryHandler(update: Update, context: CallbackContext):
         query = update.callback_query.data
         update.callback_query.answer()
-        global first_food, second_food, third_food, order_status,phone_number
-
+        global connection,phone_number
+        
         if "1" in query:
-            context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(
-                get(first_food_url).content, caption="")])
-
-            buttons = [[InlineKeyboardButton("add to buskets🥡", callback_data="first")],
-                       [InlineKeyboardButton("finish order! :(", callback_data="tugadi")]]
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     reply_markup=InlineKeyboardMarkup(buttons), text="this food costs 10000 sum 😄")
+            
+            context.bot.send_message(chat_id=update.effective_chat.id,text="successfully accepted!")
+            status_text="waiting"
+            status2="accepted"
+            connection.commit()
+            cursor = connection.cursor()
+            cursor.execute(f'update bot2 set status=? where status=?  ;',
+                            (status2,status_text))
+            connection.commit()
+        
 
         elif "2" in query:
-
-            context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(
-                get(second_food_url).content, caption="")])
-
-            buttons = [[InlineKeyboardButton("add to buskets🥡", callback_data="second")],
-                       [InlineKeyboardButton("finish order! :(", callback_data="tugadi")]]
-            a = context.bot.send_message(chat_id=update.effective_chat.id,
-                                         reply_markup=InlineKeyboardMarkup(buttons), text="this food costs 15000 sum 🍞")
-
-        elif "3" in query:
-            context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(
-                get(third_food_url).content, caption="")])
-
-            buttons = [[InlineKeyboardButton("add to buskets🥡", callback_data="third")],
-                       [InlineKeyboardButton("finish order! :(", callback_data="tugadi")]]
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     reply_markup=InlineKeyboardMarkup(buttons), text="this food costs 20000 🥪 sum")
-
-        elif "tugadi" in query:
-
             context.bot.send_message(
-                chat_id=update.effective_chat.id, text="finished")
-            now = datetime.now()
-
-            cursor = connection.cursor()
-            cursor.execute('insert into bot2 values (?,?,?,?,?,?,?,?);',
-                           (first_food, second_food, third_food, update.effective_chat.full_name, update.effective_chat.id, order_status, now,phone_number))
+                chat_id=update.effective_chat.id, text="you rejected order:(")
+            status_text="waiting"
+            status3="declined"
             connection.commit()
-            
+            cursor = connection.cursor()
+            cursor.execute(f'update bot2 set status=? where status=? ;',
+                            (status3,status_text))
+            connection.commit()
+        
+         
+    dispatcher.add_handler(CallbackQueryHandler(queryHandler))   
 
 
-
-        elif "first" in query:
-            first_food += 1
-
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"added {first_food}   to buskets 😄,if you want click again")
-
-        elif "second" in query:
-            second_food += 1
-
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"added {second_food} 🍞 to buskets ,if you want click again")
-
-        elif "third" in query:
-            third_food += 1
-
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"added {third_food}  🥪 to buskets,if you want click again")
-
-        print(
-            f"first food is {first_food}, second food {second_food}, third food {third_food}")
-    dispatcher.add_handler(CallbackQueryHandler(queryHandler))
-
- 
-
-
+   
 dispatcher.add_handler(CommandHandler("start", startCommand))
 dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
-
 updater.start_polling()
